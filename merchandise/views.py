8f27819e10ad7160403bfac671cbcf6b7ef0c131
kosiannpann <sunshainekh@gmail.com>
2021-11-21 15:13:55 +0900

    plan_list html make

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Plan
 
 # Create your views here.
+
+
+def plan_list(request):
+    plans = Plan.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'merchandise/plan_list.html', {'plans': plans})
